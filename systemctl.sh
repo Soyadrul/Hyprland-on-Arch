466a4d335f861426414de9a9d96d0298df54bd9e
Soyadrul <marco.pavanetto02@gmail.com>
2024-07-03 16:50:37 +0200

    fix: Enabled tlp.service

--- systemctl.sh
+++ systemctl.sh
@@ -4,5 +4,6 @@ sudo systemctl enable sddm
 sudo systemctl enable NetworkManager.service
 sudo systemctl enable ModemManager.service
 sudo systemctl enable bluetooth.service
+sudo systemctl enable tlp.service
 #systemctl enable bluez
 
