637ce6a5d0ce6f1ca4aa0d7bc650ec7ad4af19db
Soyadrul <marco.pavanetto02@gmail.com>
2024-09-22 16:42:14 +0200

    chore: Changed paru (AUR helper) to its bin version

--- aur.sh
+++ aur.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/bash
 
 # Variables
-Paru_download_link="https://aur.archlinux.org/cgit/aur.git/snapshot/paru.tar.gz"
-Paru_zip_folder_name="paru.tar.gz"
-Paru_folder="paru/"
+Paru_download_link="https://aur.archlinux.org/cgit/aur.git/snapshot/paru-bin.tar.gz"
+Paru_zip_folder_name="paru-bin.tar.gz"
+Paru_folder="paru-bin/"
 
 mkdir -p $HOME/Downloads/
 cd $HOME/Downloads/
@@ -15,6 +15,7 @@ tar -xvzf "${Paru_zip_folder_name}"
 
 cd "${Paru_folder}"
 
+# The -s flag is needed to automatically download and install needed dependencies that are not present in the current machine
 makepkg -s
 
 Paru_file_name=$(ls | grep x86_64.pkg.tar.zst | grep -v debug)
