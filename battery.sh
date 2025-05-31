7826286b83066cada6099bbe4bd41630135a6b2a
Soyadrul <marco.pavanetto02@gmail.com>
2025-05-31 11:56:16 +0200

    fix: Now the systemd unit works as intended

--- battery.sh
+++ battery.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 # Save the script
-sudo cp extra/battery_charge_control /usr/local/bin/battery_charge_control
-sudo chmod +x /usr/local/bin/battery_charge_control
+sudo cp extra/battery-control.sh /usr/local/bin/battery-control.sh
+sudo chmod +x /usr/local/bin/battery-control.sh
 
 # Install the systemd service
-sudo cp extra/battery_charge_control.service /etc/systemd/system/
+sudo cp extra/battery-control.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl enable battery_charge_control.service
+sudo systemctl enable battery-control.service
+sudo systemctl start battery-control.service
