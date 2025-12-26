bf60b850cf53f9cb98fb4c42eff0d493559df551
Soyadrul <118212567+Soyadrul@users.noreply.github.com>
2025-12-26 19:34:36 +0100

    Refactor AUR helper installation script

--- aur.sh
+++ aur.sh
@@ -1,22 +1,47 @@
 #! /usr/bin/bash
 
-# Variables
-Paru_download_link="https://aur.archlinux.org/cgit/aur.git/snapshot/paru-bin.tar.gz"
-Paru_zip_folder_name="paru-bin.tar.gz"
-Paru_folder="paru-bin/"
+install_aur_helper() {
+	
+	# List of AUR helpers (from input)
+	local OPTIONS=("$@")
+	
+	# Check if another AUR helper is installed
+	for helper in "${OPTIONS[@]}"; do
+		if command -v "${helper}" >/dev/null 2>&1; then
+			echo "Already installed: ${helper}"
+			exit 0 # Exits the bash script with success code if an AUR helper is already installed
+		fi
+	done
+	
+	# Choose the AUR helper
+	local CHOICE=$(gum choose --limit 1 "${OPTIONS[@]}")
+	
+	# Defaulting to yay AUR helper if no option chosen
+	if [ -z "${CHOICE}" ]; then
+		CHOICE="yay"
+	fi
+	
+	# Variables
+	local AUR_helper="${CHOICE}"
+	local Download_link="https://aur.archlinux.org/cgit/aur.git/snapshot/${AUR_helper}-bin.tar.gz"
+	local Zip_folder_name="${AUR_helper}-bin.tar.gz"
+	local Folder="${AUR_helper}-bin/"
 
-mkdir -p $HOME/Downloads/
-cd $HOME/Downloads/
+	mkdir -p $HOME/Downloads/
+	cd $HOME/Downloads/
 
-download_process=$(curl "${Paru_download_link}" --output "${Paru_zip_folder_name}")
-wait ${download_process}
+	local download_process=$(curl "${Download_link}" --output "${Zip_folder_name}")
+	wait ${download_process}
 
-tar -xvzf "${Paru_zip_folder_name}"
+	tar -xvzf "${Zip_folder_name}"
 
-cd "${Paru_folder}"
+	cd "${Folder}"
 
-# The -s flag is needed to automatically download and install needed dependencies that are not present in the current machine
-makepkg -s
+	# The -s flag is needed to automatically download and install needed dependencies that are not present in the current machine
+	makepkg -s
 
-Paru_file_name=$(ls | grep x86_64.pkg.tar.zst | grep -v debug)
-sudo pacman -U "${Paru_file_name}" --noconfirm
+	File_name=$(ls | grep x86_64.pkg.tar.zst | grep -v debug)
+	sudo pacman -U "${File_name}" --noconfirm
+	
+	echo "${AUR_helper}"
+}
