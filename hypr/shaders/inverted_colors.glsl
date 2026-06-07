83405942a214668271b0efbe3dbecdc8ce888468
Soyadrul <marco.pavanetto02@gmail.com>
2026-06-07 22:29:47 +0200

    fix: Inverted colors shader

--- hypr/shaders/inverted_colors.glsl
+++ hypr/shaders/inverted_colors.glsl
@@ -1,10 +1,13 @@
+#version 300 es
+
 precision highp float;
-varying vec2 v_texcoord;
+in vec2 v_texcoord;
 uniform sampler2D tex; // Input: Screen texture sampler (automatically provided by Hyprland)
+out vec4 fragColor;
 
 void main() {
   vec4 normal = texture2D( tex, v_texcoord); 
   vec3 inverted = vec3(1.0 - normal.r, 1.0 - normal.g, 1.0 - normal.b);
   
-  gl_FragColor = vec4(inverted, 1.0 - normal.a);
+  fragColor = vec4(inverted, 1.0 - normal.a);
 }
